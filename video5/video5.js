e35cd0b6216fbf45fa049a8c8f0158d25f62aa07
lephuchung <lephuchung003@gmail.com>
2024-02-02 01:12:33 +0700

    video5

---
+++ video5/video5.js
@@ -0,0 +1,9 @@
+let s1 = "   e   Hekaw12   ";
+let s2 = "1$2$3$4";
+let s3 = "hung hello world";
+console.log((s1.trim()).toUpperCase());
+console.log(s1.toLowerCase());
+console.log(s2.split("$"));
+let num = s2.split("$");
+console.log(num[2]);
+console.log(s3.slice(5, 10));
