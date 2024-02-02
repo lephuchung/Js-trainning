b65c6f76e8a0104ab0a8607cc25c3752a8618f1b
lephuchung <lephuchung003@gmail.com>
2024-02-03 03:54:54 +0700

    video12

---
+++ video12/video12.js
@@ -0,0 +1,13 @@
+let arr = ['MU', 'MC', 'ARS'];
+let i = 0;
+do{
+    console.log(arr[i]);
+    i++;
+}while(i<2);
+let j = 0;
+while (j<1) {
+    console.log(arr[j]);
+    j++;
+}
+console.log(i);
+console.log(j);
