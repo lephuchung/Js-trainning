b4b5e0bc675fc05224ebe940bbe69817d8e7c549
lephuchung <lephuchung003@gmail.com>
2024-02-03 03:40:03 +0700

    video11

---
+++ video11/video11.js
@@ -0,0 +1,18 @@
+console.log('hello world from js');
+// vong lap
+for (let i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+let arr = ['MU', 'MC', 'Chel'];
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+// so sanh ++i va i++
+let n = 3;
+let m = 3;
+let j = n++;
+let k = ++m;
+console.log(j);
+console.log(k); 
